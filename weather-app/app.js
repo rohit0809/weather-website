61b0b7a46360e6d589ef78d5833f496e2b96bf5d
Rohit Anand <rohit.aanand1005@gmail.com>
2019-04-26 01:13:38 +0530

    weather forecast

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,7 +2,6 @@ const request = require('request')
 
 const url = "https://api.darksky.net/forecast/6614dc7c2267f384ac1a99d86af2a8bb/37.8267,-122.4233"
 
-request({url:url}, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.hourly)
+request({url:url, json:true}, (error, response) => {
+    console.log('It is currently '+response.body.currently.temperature+' degrees out. There is a '+ response.body.currently.precipProbability +'% chance of rain.')
 })
