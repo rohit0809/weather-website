4d311aaecad37db698975343fd7befbc5d0d0680
Rohit Anand <rohit.aanand1005@gmail.com>
2019-04-26 00:28:38 +0530

    Asynchronous nature

---
+++ weather-app/app.js
@@ -0,0 +1,12 @@
+console.log('Starting.....')
+
+setTimeout(() => {
+    console.log('2 sec Timer')
+},2000)
+
+setTimeout(() => {
+    console.log('0 sec Timer')
+},0)
+
+
+console.log('Stopping....')
