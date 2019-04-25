3b2342bec17b980e12e2459a75ccd0f723c5c074
Rohit Anand <rohit.aanand1005@gmail.com>
2019-04-26 00:53:46 +0530

    Parsing JSON data

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,5 +3,6 @@ const request = require('request')
 const url = "https://api.darksky.net/forecast/6614dc7c2267f384ac1a99d86af2a8bb/37.8267,-122.4233"
 
 request({url:url}, (error, response) => {
-    console.log(response)
+    const data = JSON.parse(response.body)
+    console.log(data.hourly)
 })
