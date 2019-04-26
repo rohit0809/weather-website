e8438d497dcfd2b1fd0bbfc3f76d1f47cf74e6f0
Rohit Anand <rohit.aanand1005@gmail.com>
2019-04-27 01:55:44 +0530

    Created Geocode Utility

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,5 +1,7 @@
 const request = require('request')
+const geocode = require('./utils/geocode.js')
 
+/*
 // darknet api
 const url = "https://api.darksky.net/forecast/6614dc7c2267f384ac1a99d86af2a8bb/37.8267,-122.4233?units=si"
 
@@ -30,28 +32,9 @@ request({url:geourl, json:true}, (error, response) => {
         console.log('Lattitude is '+response.body.features[0].center[1]+' and Longitude is '+response.body.features[0].center[0])
     }
 })
+*/
 
-const geocode = (address, callback) => {
-    const geourl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) +'.json?access_token=pk.eyJ1Ijoicm9oaXQwODA5IiwiYSI6ImNqdXgydmY0aDA1Z2o0NHBncDk2ZXpzZHIifQ.gTefkGtC2RSDT21bIpaYfw&limit=1'
-
-    request({url:geourl, json:true}, (error, response) => {
-        if(error){
-            callback('Error!!!Unable to connect to api!!', undefined)
-        }
-        else if(response.body.features.length === 0){
-            callback('Error!!!Unable to search location!! Try some other strings', undefined)
-        }
-        else{
-            callback(undefined, {
-                latitude: response.body.features[0].center[1],
-                longitude: response.body.features[0].center[0],
-                location: response.body.features[0].place_name
-            })
-        }
-    })
-}
-
-geocode('Kolkata', (error, data) => {
+geocode('Patna', (error, data) => {
     console.log(error)
     console.log(data)
 })
