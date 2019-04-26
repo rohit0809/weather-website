0a5ae57e60981a3978cc66c849af2ef579a986aa
Rohit Anand <rohit.aanand1005@gmail.com>
2019-04-27 01:49:36 +0530

    Callback Abstraction

--- weather-app/app.js
+++ weather-app/app.js
@@ -31,3 +31,28 @@ request({url:geourl, json:true}, (error, response) => {
     }
 })
 
+const geocode = (address, callback) => {
+    const geourl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) +'.json?access_token=pk.eyJ1Ijoicm9oaXQwODA5IiwiYSI6ImNqdXgydmY0aDA1Z2o0NHBncDk2ZXpzZHIifQ.gTefkGtC2RSDT21bIpaYfw&limit=1'
+
+    request({url:geourl, json:true}, (error, response) => {
+        if(error){
+            callback('Error!!!Unable to connect to api!!', undefined)
+        }
+        else if(response.body.features.length === 0){
+            callback('Error!!!Unable to search location!! Try some other strings', undefined)
+        }
+        else{
+            callback(undefined, {
+                latitude: response.body.features[0].center[1],
+                longitude: response.body.features[0].center[0],
+                location: response.body.features[0].place_name
+            })
+        }
+    })
+}
+
+geocode('Kolkata', (error, data) => {
+    console.log(error)
+    console.log(data)
+})
+
