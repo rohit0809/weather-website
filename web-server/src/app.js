3af782e2b57f451b131d4e09259026619ccedfa3
Rohit Anand <rohit.aanand1005@gmail.com>
2019-04-28 21:09:48 +0530

    Handle Bar Applications

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -7,6 +7,27 @@ const publicDirectory = path.join(__dirname, '../public')
 app.set('view engine', 'hbs')
 app.use(express.static(publicDirectory))
 
+app.get('',(req,res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Rohit Anand'
+    })
+})
+
+app.get('/about',(req,res) => {
+    res.render('about',{
+        title: 'About Me',
+        name: 'Rohit Anand'
+    })
+})
+
+app.get('/help',(req,res) => {
+    res.render('help',{
+        title: 'Help Page',
+        name: 'Rohit Anand'
+    })
+})
+
 
 app.get('/weather',(req,res) => {
     res.send([{
