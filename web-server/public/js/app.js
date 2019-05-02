af9e87bc70b8900495dd32e612ebde1b459dae45
Rohit Anand <rohit.aanand1005@gmail.com>
2019-05-03 02:09:32 +0530

    Weather App with UI version

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,26 @@
 console.log('Client side JS file is running!!!')
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageone = document.querySelector('#message-1')
+const messagetwo = document.querySelector('#message-2')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    messageone.textContent = 'Loading Results......'
+    messagetwo.textContent = ''
+
+    fetch('http://localhost:3000/weather?address='+ location).then((response) => {
+        response.json().then((data) => {
+            if(data.error){
+                messageone.textContent = data.error
+            }else{
+                messageone.textContent = data.location
+                messagetwo.textContent = data.forecast
+            }
+        })
+    })
+})
