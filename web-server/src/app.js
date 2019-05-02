5244ee2a386c4888c9a90a22f0f28c70bd01ca93
Rohit Anand <rohit.aanand1005@gmail.com>
2019-05-02 22:29:13 +0530

    Linking web-server with weather-app

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const path = require('path')
 const hbs = require('hbs')
+const geocode = require('./utils/geocode.js')
+const forecast = require('./utils/forecast.js')
 
 const app = express()
 
@@ -47,10 +49,25 @@ app.get('/weather',(req,res) => {
         })
     }
 
-    res.send({
-        forecast: 'It is snowy weather!!',
-        location: req.query.address 
-    })
+    geocode(req.query.address, (error, {latitude, longitude, location}) => {
+        if(error){
+            return res.send({
+                error
+            })
+        }
+         forecast(latitude, longitude, (error, forecastData) => {
+             if(error){
+                 return res.send({
+                    error
+                })
+             }
+             res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address 
+            })
+         })
+     })
 })
 
 app.get('/help/*', (req,res) => {
